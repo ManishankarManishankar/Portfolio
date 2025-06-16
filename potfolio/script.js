3af8234a86d57427b8b359eeb08ec028d5657f76
ManishankarManishankar <manishanker.kcm333@gmail.com>
2025-06-16 22:20:55 +0530

    Add files via upload

---
+++ potfolio/script.js
@@ -0,0 +1,11 @@
+function menu(){
+    // var menu=document.getElementById("menu-icon");
+    var headpart=document.getElementById("head-part")
+    if(headpart.style.display==="none"){
+        headpart.style.display= "block"
+        headpart.style.transitionDuration
+    }
+    else{
+        headpart.style.display="none"
+    }
+}
